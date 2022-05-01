ad5e4ff00998f969f3f597341ed3ef366e4f09d5
Yijiang Wang <lincvic@yahoo.com>
2022-05-01 23:50:57 +0100

    Check user is existed or not

--- src/router/user-router.js
+++ src/router/user-router.js
@@ -16,30 +16,40 @@ router.post('/createUser', (req, res) => {
             msg: `Input error`
         })
     }
-    const newUser = new User(req.body.uid,
-        req.body.email,
-        req.body.first_name,
-        req.body.last_name,
-        req.body.role,
-        req.body.deliver_status,
-        req.body.address_first_line,
-        req.body.address_second_line,
-        req.body.city,
-        req.body.country,
-        req.body.postcode
-    )
-    DAO.createNewUser(newUser).then(() => {
-        res.status(200).json({
-            result: true,
-            msg: `User ${newUser.email} created`
-        })
-    }).catch(err => {
-        console.log(err.message)
-        res.status(400).json({
-            result: false,
-            msg: `User ${newUser.email} create failed`
-        })
+    DAO.getUserByUID(req.body.uid).then((it) =>{
+        if(!it.data()){
+            const newUser = new User(req.body.uid,
+                req.body.email,
+                req.body.first_name,
+                req.body.last_name,
+                req.body.role,
+                req.body.deliver_status,
+                req.body.address_first_line,
+                req.body.address_second_line,
+                req.body.city,
+                req.body.country,
+                req.body.postcode
+            )
+            DAO.createNewUser(newUser).then(() => {
+                res.status(200).json({
+                    result: true,
+                    msg: `User ${newUser.email} created`
+                })
+            }).catch(err => {
+                console.log(err.message)
+                res.status(400).json({
+                    result: false,
+                    msg: `User ${newUser.email} create failed`
+                })
+            })
+        }else{
+            res.status(400).json({
+                result: false,
+                msg: `User ${req.body.uid} already exist`
+            })
+        }
     })
+
 })
 
 router.post('/getUserByUID', (req, res) => {
